85008d2ccdfb055f3c1667ce6b0dd6abac1d62b4
Octavio Mejia <arkord1@gmail.com>
2021-01-25 10:15:03 -0600

    assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
